04c0301580486857215e8a7b1cd6ea7f8154a975
Paarth Tandon <paarthri@gmail.com>
2022-12-05 00:22:12 -0500

    custom reward

--- experiements/starter_team/env.py
+++ experiements/starter_team/env.py
@@ -69,3 +69,53 @@ class RLPlayerOnlyWins(Gen4EnvSinglePlayer):
             np.array(high, dtype=np.float32),
             dtype=np.float32,
         )
+
+class RLPlayerCustom(Gen4EnvSinglePlayer):
+
+    def calc_reward(self, last_battle, current_battle) -> float:
+        reward = 0
+
+        last_fainted = 0
+        for mon in last_battle.opponent_team.values():
+            if mon.fainted:
+                last_fainted += 1
+        current_fainted = 0
+        for mon in current_battle.opponent_team.values():
+            if mon.fainted:
+                current_fainted += 1
+        
+        reward = reward + ((current_fainted - last_fainted) * 10)
+        
+        if current_battle.won:
+            reward += 100
+        elif current_battle.lost:
+            reward -= 100
+        
+        return reward
+
+    def embed_battle(self, battle):
+        def hp_bin(hp):
+            return int(3 * hp)
+
+        pokemon = {
+            'charizard': 0,
+            'venusaur': 1,
+            'blastoise': 2
+        }
+
+        my_mon = pokemon[battle.active_pokemon.species]
+        opponent_mon = pokemon[battle.opponent_active_pokemon.species]
+
+        my_hp = hp_bin(battle.active_pokemon.current_hp_fraction)
+        opponent_hp = hp_bin(battle.opponent_active_pokemon.current_hp_fraction)
+
+        return [my_mon, opponent_mon, my_hp, opponent_hp]
+    
+    def describe_embedding(self) -> Space:
+        low = [0, 0, 0, 0]
+        high = [0, 0, 3, 3]
+        return Box(
+            np.array(low, dtype=np.float32),
+            np.array(high, dtype=np.float32),
+            dtype=np.float32,
+        )
---
+++ experiements/starter_team/q_learning/heuristics_ok.py
@@ -0,0 +1,55 @@
+import sys
+sys.path.append('..')
+
+from poke_env.player import SimpleHeuristicsPlayer
+from poke_env.player_configuration import PlayerConfiguration
+from env import RLPlayerCustom
+from algo import QLearning
+from matplotlib import pyplot as plt
+import json
+
+EXPERIEMENT_NAME = 'heuristics_ok_ql'
+EXPERIEMENT_PATH = f'results/{EXPERIEMENT_NAME}'
+TRAIN_STEPS = 500_000
+EVAL_STEPS = 100
+
+with open('../team.txt', 'r') as teamf:
+    team = teamf.read()
+
+pc = PlayerConfiguration(f'heur_ok_ql_op', '')
+player = SimpleHeuristicsPlayer(
+    battle_format="gen8ou",
+    team=team,
+    player_configuration=pc
+)
+
+pc = PlayerConfiguration(f'heur_ok_ql', '')
+rl_player = RLPlayerCustom(
+    opponent=player,
+    battle_format="gen8ou",
+    team=team,
+    player_configuration=pc
+)
+
+ql = QLearning(gamma=1, e_start=1, a_start=1, e_dec=1/(TRAIN_STEPS/2), a_dec=1/(TRAIN_STEPS/2), min_e=0.01, min_a=0.01)
+train_results = ql.train(rl_player, TRAIN_STEPS)
+
+plt.title(f'QL vs. {EXPERIEMENT_NAME}')
+plt.xlabel('steps')
+plt.ylabel('reward')
+plt.plot(train_results['steps'], train_results['rewards'])
+plt.savefig(f'{EXPERIEMENT_PATH}/training.png')
+
+with open(f'{EXPERIEMENT_PATH}/train.json', 'w') as f:
+    json.dump({
+        'steps': train_results['steps'],
+        'rewards': train_results['rewards']
+    }, f)
+
+test_results = ql.eval(rl_player, EVAL_STEPS)
+print(f'Eval: {test_results["n_wins"]}/{test_results["n_battles"]}')
+with open(f'{EXPERIEMENT_PATH}/eval.json', 'w') as f:
+    json.dump(test_results, f)
+
+ql.save_q(f'{EXPERIEMENT_PATH}/q.pickle')
+rl_player.close()
